a84000292ff60735325a606265d280db566b7085
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2015-12-06 17:31:06 +0000

    First stab at a script to do my initial Raspberry Pi setup

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p ~/develop
+cd       ~/develop
+
+git config --global user.name  ClareMacrae
+git config --global user.email "github@cfmacrae.fastmail.co.uk"
+git clone https://github.com/claremacrae/raspi_code.git
