a4ada8595b8465226df36f041a6f584718919b01
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2015-12-06 17:35:06 +0000

    Also clone my arduino_code library

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ cd       ~/develop
 git config --global user.name  ClareMacrae
 git config --global user.email "github@cfmacrae.fastmail.co.uk"
 git clone https://github.com/claremacrae/raspi_code.git
+git clone https://github.com/claremacrae/arduino_code
