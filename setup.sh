ed263c26687f011ecce477a34369ce0a17de23a8
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2015-12-09 20:42:16 +0000

    Clone more of my repos, and allow the script to be run more than once.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+set -u
+
 mkdir -p ~/develop
 cd       ~/develop
 
 git config --global user.name  ClareMacrae
 git config --global user.email "github@cfmacrae.fastmail.co.uk"
-git clone https://github.com/claremacrae/raspi_code.git
-git clone https://github.com/claremacrae/arduino_code
+
+repos="raspi_code raspi_setup raspi_hat_data arduino_code"
+for repo in $repos
+do
+    if [ ! -d "$repo" ]; then
+        echo Creating $repo
+        git clone https://github.com/claremacrae/${repo}.git
+    fi
+done
+
