0ab2383787fa40fe94e4f6cb87b8797c0a7c6556
ClareMacrae <github@cfmacrae.fastmail.co.uk>
2017-09-02 22:13:46 +0000

    Set the Mercurial username, if ~/.hgrc doesn't exist. This saves worrying about edited pre-existing file, for simplicity.

--- setup.sh
+++ setup.sh
@@ -8,6 +8,14 @@ cd       ~/develop
 git config --global user.name  ClareMacrae
 git config --global user.email "github@cfmacrae.fastmail.co.uk"
 
+HGRC=~/.hgrc
+if [ ! -f $HGRC ]; then
+    cat > $HGRC <<EOF
+[ui]
+username = Clare Macrae <github@cfmacrae.fastmail.co.uk>
+EOF
+fi
+
 repos="raspi_code raspi_setup raspi_hat_data arduino_code"
 for repo in $repos
 do
