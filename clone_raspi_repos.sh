cc73e4eef5ea00194a39f86050c8b0d97a618dfa
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2017-12-03 12:30:15 +0000

    Move code for cloning repos out to separate script

---
+++ clone_raspi_repos.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -u
+
+mkdir -p ~/develop
+cd       ~/develop
+
+repos="raspi_code raspi_setup raspi_hat_data arduino_code"
+for repo in $repos
+do
+    if [ ! -d "$repo" ]; then
+        echo Creating $repo
+        git clone https://github.com/claremacrae/${repo}.git
+    fi
+done
