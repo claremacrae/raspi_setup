bea90118574b3a8dc418f720c262df58bd8ef9db
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2017-12-03 12:31:04 +0000

    Move code for cloning repos out to separate script

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 
 set -u
 
-mkdir -p ~/develop
-cd       ~/develop
-
 git config --global user.name  ClareMacrae
 git config --global user.email "github@cfmacrae.fastmail.co.uk"
 
@@ -15,13 +12,3 @@ if [ ! -f $HGRC ]; then
 username = Clare Macrae <github@cfmacrae.fastmail.co.uk>
 EOF
 fi
-
-repos="raspi_code raspi_setup raspi_hat_data arduino_code"
-for repo in $repos
-do
-    if [ ! -d "$repo" ]; then
-        echo Creating $repo
-        git clone https://github.com/claremacrae/${repo}.git
-    fi
-done
-
